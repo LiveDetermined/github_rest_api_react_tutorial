5de79c0eeff7dda513456f7155640a9f117694b5
LiveDetermined <tandy09@gmail.com>
2022-02-05 17:37:39 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import logo from "./logo.svg";
+import { useEffect, useState } from "react";
+import "./App.css";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+
+function App() {
+  const [avatarURL, setAvatarURL] = useState();
+  const [githubUsername, setGitHubUsername] = useState();
+  const [repoData, setRepoData] = useState();
+
+  async function repoDataURL() {
+    //Get repo data about github user
+    await fetch("https://api.github.com/users/LiveDetermined/repos")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          console.log(36, result);
+          const list = result.map((item) => (
+            <div className="text-center">
+              <a target="_blank" href={item.svn_url}>
+                {item.name}
+              </a>
+            </div>
+          ));
+          setRepoData(list);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/Livedetermined")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          console.log(result);
+          setAvatarURL(result.avatar_url);
+          setGitHubUsername(result.login);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }, []);
+  return (
+    <div className="App w-100 min-vh-100 justify-content-center align-items-center d-flex flex-column">
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src={avatarURL} />
+        <Card.Body>
+          <Card.Title>{githubUsername}</Card.Title>
+
+          <Button variant="primary" onClick={repoDataURL}>
+            List my public repos!
+          </Button>
+        </Card.Body>
+      </Card>
+      {repoData}
+    </div>
+  );
+}
+
+export default App;
